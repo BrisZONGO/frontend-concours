cac465a1252fd5b9fc48e25e9414b5e68a29512d
zobridel <efptppaf@gmail.com>
2026-04-18 15:16:47 +0000

    add mobile money frontend

---
+++ src/pages/Success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div className="container">
+      <h2>✅ Paiement en cours de validation</h2>
+      <p>Merci. Votre accès sera activé après confirmation.</p>
+    </div>
+  );
+}
